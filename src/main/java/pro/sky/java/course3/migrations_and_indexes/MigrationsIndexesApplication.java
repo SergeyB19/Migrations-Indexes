44d2f8b900a5012515a39f891a05832ecdc5a385
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-10-24 12:21:58 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course3/migrations_and_indexes/MigrationsIndexesApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course3.migrations_and_indexes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationsIndexesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationsIndexesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/java/course3/migrations_and_indexes/MigrationsIndexesApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course3.migrations_and_indexes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationsIndexesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
